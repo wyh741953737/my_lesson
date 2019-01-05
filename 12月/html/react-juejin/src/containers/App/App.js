c4dbd2892b3a10199544816b9f6a4d378daa71b8
wyh741953737 <741953737@qq.com>
2019-01-05 10:42:39 +0800

    juejin

--- 12月/html/react-juejin/src/containers/App/App.js
+++ 12月/html/react-juejin/src/containers/App/App.js
@@ -16,13 +16,13 @@ class App extends Component {
         <div className="App-body">
           <div className="welcome-view">
             <div className="category-nav">
-              1adasdasdasdasdasd1adasdasdasdasdasd1adasdasdasdasdasd1adasdasdasdasdasd
+              adasdasdasdasdasd1adasdasdasdasdasd
             </div>
             <div className="main">
-              21adasdasdasdasdasd1adasdasdasdasdasd1adasdasdasdasdasd1adasdasdasdasdasd1adasdasdasdasdasd
+              d1adasdasdasdasdasd1adasdasdasdasdasd
             </div>
             <div className="sidebar">
-              31adasdasdasdasdasd1adasdasdasdasdasd1adasdasdasdasdasd1adasdasdasdasdasd1adasdasdasdasdasd
+              d1adasdasdasdasdasd1adasdasdasdasdasd
             </div>
           </div>
         </div>
