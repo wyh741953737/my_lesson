851d12dcbbf3b6e22d598b035fe4eb6b29804a2a
wyh741953737 <741953737@qq.com>
2019-01-05 15:36:28 +0800

    react-ts-webpack

---
+++ 12月/html/react-te-webpack/src/App.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+import {Header,About} from './components'
+export const App:
+React.StatelessComponent<{}>=()=>{
+    return <div>
+        <Header/>
+        <About/>
+    </div>
+}
